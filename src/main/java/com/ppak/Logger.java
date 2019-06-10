3f545ff703d0e49047e626e0da670566b9a96343
zbychhhu <grzegorzmarchewka351@gmail.com>
2019-06-10 20:36:46 +0200

    add Logger class(Singleton)

---
+++ src/main/java/com/ppak/Logger.java
@@ -0,0 +1,36 @@
+package com.ppak;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class Logger {
+
+    private static Logger instance;
+
+    private Logger() {
+    }
+
+    public static Logger getInstance() {
+        if (instance == null) {
+            instance = new Logger();
+        }
+        return instance;
+    }
+
+    private String date() {
+        LocalDateTime dateTime = LocalDateTime.now();
+        DateTimeFormatter format = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm");
+        return dateTime.format(format);
+    }
+
+    public void info(String info) {
+        System.out.println(date() + " INFO: " + info);
+    }
+
+    public void error(String error) {
+        System.out.println(date() + " ERROR: " + error);
+    }
+
+
+
+}
