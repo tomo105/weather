ec4b09b7b0db995c03727b5278ce163cca844522
MPomaranski <48623973+MPomaranski@users.noreply.github.com>
2019-06-12 01:22:47 +0200

    Last but not least xD

--- src/main/java/com/ppak/view/Menu.java
+++ src/main/java/com/ppak/view/Menu.java
@@ -21,7 +21,7 @@ public class Menu implements ActionListener {
     private Weather tmpWeather;
 
     JMenu menu, submenu;
-    JLabel jLabel;
+    JLabel jLabel, deg, rest, rest2, rest3, rest4, rest5, miasto;
     JMenuItem i1, i2, i3, i4, i5, i6;
     JFrame frame = new JFrame("Pogoda");
     JButton leftButton;
@@ -35,9 +35,38 @@ public class Menu implements ActionListener {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         JMenuBar mb = new JMenuBar();
 
+        Font font = new Font(Font.SANS_SERIF, Font.PLAIN, 90);
+        Font font1 = new Font(Font.SANS_SERIF, Font.PLAIN, 24);
+
         jLabel = new JLabel("");
-        jLabel.setBounds(10, 10, 560, 560);
-        jLabel.setSize(900, 600);
+        jLabel.setBounds(600, -150, 900, 600);
+
+        miasto = new JLabel("KRAKÓW");
+        miasto.setBounds(50, -190, 500, 500);
+        miasto.setFont(font);
+
+        deg = new JLabel("");
+        deg.setBounds(300, 150, 560, 560);
+        deg.setSize(450, 150);
+        rest = new JLabel("");
+        rest.setBounds(100, 50, 560, 560);
+        rest.setSize(450, 150);
+        rest2 = new JLabel("");
+        rest2.setBounds(100, 100, 560, 560);
+        rest2.setSize(250, 150);
+        rest3 = new JLabel("");
+        rest3.setBounds(100, 150, 560, 560);
+        rest3.setSize(250, 150);
+        rest4 = new JLabel("");
+        rest4.setBounds(100, 200, 560, 560);
+        rest4.setSize(250, 150);
+        rest5 = new JLabel("");
+        rest5.setBounds(100, 250, 560, 560);
+        rest5.setSize(250, 150);
+
+        rest.setFont(font1);
+
+        deg.setFont(font);
 
         frame.setMinimumSize(new Dimension(400, 300));
         frame.setPreferredSize(new Dimension(900, 600));
@@ -45,18 +74,18 @@ public class Menu implements ActionListener {
 
         rightButton = new JButton("+3H");
         leftButton = new JButton("-3H");
-        leftButton.setBounds(50,100,95,30);
-        rightButton.setBounds(300,100,95,30);
+        leftButton.setBounds(150, 400, 150, 70);
+        rightButton.setBounds(550, 400, 150, 70);
 
         menu = new JMenu("Menu");
         submenu = new JMenu("Wybierz dzień");
 
         i1 = new JMenuItem("O aplikacji");
-        i2 = new JMenuItem("Dzisiaj");
-        i3 = new JMenuItem("Jutro");
-        i4 = new JMenuItem("Pojutrze");
-        i5 = new JMenuItem("3 dni na przód");
-        i6 = new JMenuItem("4 dni na przód");
+        i2 = new JMenuItem(date(0));
+        i3 = new JMenuItem(date(1));
+        i4 = new JMenuItem(date(2));
+        i5 = new JMenuItem(date(3));
+        i6 = new JMenuItem(date(4));
 
         i1.addActionListener(this);
         i2.addActionListener(this);
@@ -77,6 +106,13 @@ public class Menu implements ActionListener {
         menu.add(submenu);
         mb.add(menu);
         frame.add(jLabel);
+        frame.add(deg);
+        frame.add(rest);
+        frame.add(rest2);
+        frame.add(rest3);
+        frame.add(rest4);
+        frame.add(rest5);
+        frame.add(miasto);
         frame.add(leftButton);
         frame.add(rightButton);
         frame.setJMenuBar(mb);
@@ -85,45 +121,64 @@ public class Menu implements ActionListener {
         frame.setVisible(true);
         tmpWeather = chooseFirstWeatherInDay(0);
         pathFromDB(checkImage(tmpWeather.getDescription()));
+        print();
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
 
+
         Object source = e.getSource();
+        print();
         if (source == i2) {
             tmpWeather = chooseFirstWeatherInDay(0);
             pathFromDB(checkImage(tmpWeather.getDescription()));
-        }
-        else if (source == i3){
-            tmpWeather =chooseFirstWeatherInDay(1);
+        } else if (source == i3) {
+            tmpWeather = chooseFirstWeatherInDay(1);
             pathFromDB(checkImage(tmpWeather.getDescription()));
-        }
-        else if (source == i4){
-            tmpWeather =chooseFirstWeatherInDay(2);
+        } else if (source == i4) {
+            tmpWeather = chooseFirstWeatherInDay(2);
             pathFromDB(checkImage(tmpWeather.getDescription()));
-        }
-        else if (source == i5){
-            tmpWeather =chooseFirstWeatherInDay(3);
+        } else if (source == i5) {
+            tmpWeather = chooseFirstWeatherInDay(3);
             pathFromDB(checkImage(tmpWeather.getDescription()));
-        }
-        else if (source == i6){
-            tmpWeather =chooseFirstWeatherInDay(4);
+        } else if (source == i6) {
+            tmpWeather = chooseFirstWeatherInDay(4);
             pathFromDB(checkImage(tmpWeather.getDescription()));
+
         }
-        if (source == rightButton){
+        if (source == i1)
+            JOptionPane.showMessageDialog(i1, "Apka pogoda, dobra jak jagoda");
+
+        if (source == rightButton) {
             tempId = tmpWeather.getId();
-            pathFromDB(checkImage(list.get(++tempId).getDescription()));
+            if (++tempId > 39) {
+                tempId = 39;
+            }
+            pathFromDB(checkImage(list.get(tempId).getDescription()));
             tmpWeather = list.get(tempId);
 
-        }
-        else if (source == leftButton){
+        } else if (source == leftButton) {
             tempId = tmpWeather.getId();
-            pathFromDB(checkImage(list.get(--tempId).getDescription()));
+            if (--tempId < 0) {
+                tempId = 0;
+            }
+            pathFromDB(checkImage(list.get(tempId).getDescription()));
             tmpWeather = list.get(tempId);
         }
 
 
+    }
+
+    private void print() {
+        double temperature = convertFromKelvinToCelsius(tmpWeather.getTemp());
+
+        deg.setText(String.format("%.1f", temperature) + " C");
+        rest.setText("Data: " + tmpWeather.getData());
+        rest2.setText("Ciśnienie: " + tmpWeather.getPressure() + "hPa");
+        rest3.setText("Prędkość wiatru: " + tmpWeather.getSpeed() + "km/h");
+        rest4.setText("Kąt wiatru: " + tmpWeather.getDeg());
+        rest5.setText("Wilgotność: " + tmpWeather.getHumidity() + "%");
     }
 
     private void pathFromDB(String path) {
@@ -150,7 +205,7 @@ public class Menu implements ActionListener {
 
         Weather weather = new Weather();
         for (int i = 0; i < list.size(); i++) {
-            if (list.get(i).getData().substring(0,10).equals(data)) {
+            if (list.get(i).getData().substring(0, 10).equals(data)) {
                 weather = list.get(i);
                 break;
             }
@@ -161,27 +216,22 @@ public class Menu implements ActionListener {
 
     }
 
-    private Double convertFromKelvinToCelsius(Double kelvin){
-        return kelvin-273.15;
+    private Double convertFromKelvinToCelsius(Double kelvin) {
+        return kelvin - 273.15;
     }
 
-    private String checkImage(String description){
-        if (description.equals("light rain")){
+    private String checkImage(String description) {
+        if (description.equals("light rain")) {
             return "deszcz.png";
-        }
-        else if (description.equals("clear sky")){
+        } else if (description.equals("clear sky")) {
             return "clear_sky.png";
-        }
-        else if (description.equals("few clouds")){
+        } else if (description.equals("few clouds")) {
             return "za_chmura.png";
-        }
-        else if (description.equals("broken clouds")){
+        } else if (description.equals("broken clouds")) {
             return "dwiechmury.png";
-        }
-        else if (description.equals("overcast clouds")){
+        } else if (description.equals("overcast clouds")) {
             return "ciemnachmura.png";
-        }
-        else if (description.equals("scattered clouds")){
+        } else if (description.equals("scattered clouds")) {
             return "za_chmura.png";
         }
         return "";
